aa982d227d0e31dbc90d322fc0290646e96f2616
MRraveN <scitaxapp@gmail.com>
2020-10-04 19:31:57 +0300

    task_8_2.py optimized

--- Lesson8/task_8_2.py
+++ Lesson8/task_8_2.py
@@ -5,12 +5,16 @@ def count_upper_lower_case(string_arg: Optional[str]) -> Optional[dict]:
     if not isinstance(string_arg, str):
         print('Error! String is expected')
         return None
-    lower_upper_counter = {'lower': 0, 'upper': 0}
-    for _ in string_arg:
-        if _.islower():
-            lower_upper_counter['lower'] += 1
-        if _.isupper():
-            lower_upper_counter['upper'] += 1
+    lower_case_count = 0
+    upper_case_count = 0
+    lower_upper_counter = {}
+    for current_letter in string_arg:
+        if current_letter.islower():
+            lower_case_count += 1
+        if current_letter.isupper():
+            upper_case_count += 1
         else:
             pass
+    lower_upper_counter['lower'] = lower_case_count
+    lower_upper_counter['upper'] = upper_case_count
     return lower_upper_counter
