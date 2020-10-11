4197f9789f6b0edc7c63c01a35572f21f6a4c8f5
MRraveN <scitaxapp@gmail.com>
2020-10-11 14:42:22 +0300

    fixed double_keys_2 func

--- Lesson9/task_9_2.py
+++ Lesson9/task_9_2.py
@@ -4,6 +4,6 @@ double_keys_1 = lambda **kwargs: {
 }
 
 double_keys_2 = lambda **kwargs: {
-    f'{key_in_func}{key_in_func}': arg_in_func
+    f'{key_in_func}*2': arg_in_func
     for key_in_func, arg_in_func in kwargs.items()
 }
