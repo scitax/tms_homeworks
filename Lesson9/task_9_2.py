bbabd93da35287cbe7f5433ef51a79fa4510c56e
MRraveN <scitaxapp@gmail.com>
2020-11-01 17:56:39 +0300

    restrucrured data

--- Lesson9/task_9_2.py
+++
@@ -1,9 +0,0 @@
-double_keys_1 = lambda **kwargs: {
-    f'{key_in_func}{key_in_func}': kwargs[key_in_func]
-    for key_in_func in kwargs
-}
-
-double_keys_2 = lambda **kwargs: {
-    f'{key_in_func}*2': arg_in_func
-    for key_in_func, arg_in_func in kwargs.items()
-}
--- Lesson9/task_9_4.py
+++
@@ -1,9 +0,0 @@
-def function_args_reverse(in_function):
-    def revers_args(*args):
-        return in_function(args[::-1])
-    return revers_args
-
-
-@function_args_reverse
-def any_func(*args):
-    return args
